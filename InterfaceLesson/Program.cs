0d3f97d83e43238c786efb2f45e4d82bc055d24c
Greg Doud <gpdoud@gmail.com>
2021-05-24 10:59:52 -0400

    Add project files.

---
+++ InterfaceLesson/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace InterfaceLesson {
+    class Program {
+        static void Main(string[] args) {
+
+            var pb = new PitBull();
+            var ch = new Chihuahua();
+            var sh = new Shibainu();
+
+            var dogs = new IPrint[] { pb, ch, sh };
+
+            foreach(var dog in dogs) {
+                dog.Print();
+            }
+        }
+    }
+}
---
+++ InterfaceLesson/Shibainu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceLesson {
+    class Shibainu : IPrint {
+        public string Characteristic { get; set; } = "Stand-offish";
+        public string Type { get; set; } = "Shibainu";
+
+        public void Print() {
+            Console.WriteLine($"This dog is a {Type} and it is {Characteristic}");
+        }
+    }
+}
